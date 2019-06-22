548dc41cf73075e161ecc87d72496ccde117e864
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 21:19:52 +0300

    Added url(), to fetch create_post() and delete_post() view functions (from part-2)

--- part3/talk_project/talk/urls.py
+++ part3/talk_project/talk/urls.py
@@ -5,6 +5,8 @@ from talk import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^create_post/$', views.create_post),
+    url(r'^delete_post/$', views.delete_post),
     # api
     url(r'^api/v1/posts/$', views.post_collection),
     url(r'^api/v1/posts/(?P<pk>[0-9]+)$', views.post_element),
