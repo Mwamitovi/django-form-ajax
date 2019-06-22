5f7a2cefde3045a537984ac1b4319d7f058810d0
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 21:20:46 +0300

    Define create_post() and delete_post() view functions (from part-2)

--- part3/talk_project/talk/views.py
+++ part3/talk_project/talk/views.py
@@ -1,6 +1,7 @@
 # talk/views.py
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
+import json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -12,6 +13,50 @@ from talk.serializers import PostSerializer
 def home(request):
     tmpl_vars = {'form': PostForm()}
     return render(request, 'talk/index.html', tmpl_vars)
+
+
+def create_post(request):
+    if request.method == 'POST':
+        post_text = request.POST.get('the_post')
+        response_data = {}
+
+        post = Post(text=post_text, author=request.user)
+        post.save()
+
+        response_data['result'] = 'Create post successful!'
+        response_data['postpk'] = post.pk
+        response_data['text'] = post.text
+        response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
+        response_data['author'] = post.author.username
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
+
+
+def delete_post(request):
+    if request.method == 'DELETE':
+        post = Post.objects.get(
+            pk=int(QueryDict(request.body).get('postpk'))
+        )
+        post.delete()
+
+        response_data = {'msg': 'Post was deleted.'}
+
+        response = json.dumps(response_data)
+    else:
+        response = json.dumps({"nothing to see": "this isn't happening"})
+
+    return HttpResponse(
+        response,
+        content_type="application/json"
+    )
 
 
 @api_view(['GET'])
