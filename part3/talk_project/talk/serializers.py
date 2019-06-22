ae2de27997833a7f9ee4b59920fc1a94c939800d
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 19:52:23 +0300

    Create "talk.serializers" module, and define PostSerializer to declare model fields to "serializer"

---
+++ part3/talk_project/talk/serializers.py
@@ -0,0 +1,9 @@
+# talk/serializers.py
+from rest_framework import serializers
+from talk.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'text', 'created', 'updated')
