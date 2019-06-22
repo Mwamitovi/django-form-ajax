634a2fb8b3f5805c25e02d1c7a9714f726dc616c
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 20:12:40 +0300

    Update "talk.urls" module., add url() to fetch post_collection() and post_element()

--- part3/talk_project/talk/urls.py
+++ part3/talk_project/talk/urls.py
@@ -5,4 +5,7 @@ from talk import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    # api
+    url(r'^api/v1/posts/$', views.post_collection),
+    url(r'^api/v1/posts/(?P<pk>[0-9]+)$', views.post_element),
 ]
