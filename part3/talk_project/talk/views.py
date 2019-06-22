e95544f520f2f4bbe095dce088353106e7574fe6
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 20:01:51 +0300

    Update "talk.views" module, define post_collection() (with @api_view(["GET"]) decorator) returns all posts (serialized data)

--- part3/talk_project/talk/views.py
+++ part3/talk_project/talk/views.py
@@ -1,8 +1,22 @@
 # talk/views.py
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from talk.models import Post
 from talk.forms import PostForm
+from talk.serializers import PostSerializer
 
 
 def home(request):
     tmpl_vars = {'form': PostForm()}
     return render(request, 'talk/index.html', tmpl_vars)
+
+
+@api_view(['GET'])
+def post_collection(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
