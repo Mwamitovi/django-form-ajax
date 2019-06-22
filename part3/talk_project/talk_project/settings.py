25dd5495ac8e9e52fc4e29f2d17b809a17cc82cd
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 19:20:09 +0300

    Added "talk" to INSTALLED_APPS

--- part3/talk_project/talk_project/settings.py
+++ part3/talk_project/talk_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b!#eqj9cdh2+&ta6@r&o-#giz+#@8idj8wixi_5*v-3egf&)yt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'talk',
 ]
 
 MIDDLEWARE = [
