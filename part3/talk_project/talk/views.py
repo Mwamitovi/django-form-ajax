a19310abdaf05306a411df424fcc6194832b2b5f
Martin Matovu <matovu.synergy@gmail.com>
2019-06-22 19:22:10 +0300

    Define home(0 view function, to render the PostForm() using the talk/index.html (to be defined next)

--- part3/talk_project/talk/views.py
+++ part3/talk_project/talk/views.py
@@ -1,3 +1,8 @@
+# talk/views.py
 from django.shortcuts import render
+from talk.forms import PostForm
 
-# Create your views here.
+
+def home(request):
+    tmpl_vars = {'form': PostForm()}
+    return render(request, 'talk/index.html', tmpl_vars)
