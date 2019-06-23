13b987bf7e638b247ee691fba2b71f4d5dda8c72
Martin Matovu <matovu.synergy@gmail.com>
2019-06-23 14:10:44 +0300

    Added "127.0.0.1" to ALLOWED_HOSTS

--- part3/talk_project/talk_project/settings.py
+++ part3/talk_project/talk_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b!#eqj9cdh2+&ta6@r&o-#giz+#@8idj8wixi_5*v-3egf&)yt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
