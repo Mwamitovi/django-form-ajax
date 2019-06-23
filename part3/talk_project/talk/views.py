9605d3374a56434ee19e045f3d4e4f31d36c6e42
Martin Matovu <matovu.synergy@gmail.com>
2019-06-23 15:29:51 +0300

    Remove the create_post() and delete_post(), since this functionality is to be handled by the (api) view functions i.e. post_collection() and post_element()

--- part3/talk_project/talk/views.py
+++ part3/talk_project/talk/views.py
@@ -16,50 +16,6 @@ def home(request):
     return render(request, 'talk/index.html', tmpl_vars)
 
 
-def create_post(request):
-    if request.method == 'POST':
-        post_text = request.POST.get('the_post')
-        response_data = {}
-
-        post = Post(text=post_text, author=request.user)
-        post.save()
-
-        response_data['result'] = 'Create post successful!'
-        response_data['postpk'] = post.pk
-        response_data['text'] = post.text
-        response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-        response_data['author'] = post.author.username
-
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
-    else:
-        return HttpResponse(
-            json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json"
-        )
-
-
-def delete_post(request):
-    if request.method == 'DELETE':
-        post = Post.objects.get(
-            pk=int(QueryDict(request.body).get('postpk'))
-        )
-        post.delete()
-
-        response_data = {'msg': 'Post was deleted.'}
-
-        response = json.dumps(response_data)
-    else:
-        response = json.dumps({"nothing to see": "this isn't happening"})
-
-    return HttpResponse(
-        response,
-        content_type="application/json"
-    )
-
-
 @api_view(['GET', 'POST'])
 def post_collection(request):
     if request.method == 'GET':
