945fc352d1f526cf6b4c9e7c2461976defae2a52
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 17:46:47 +0300

    Refactor post_element() to use (django-inbuilt) get_object_or_404() to get a "Post" or raise a 404 error

--- part4/talk_project/talk/views.py
+++ part4/talk_project/talk/views.py
@@ -1,5 +1,5 @@
 # talk/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, QueryDict
 import json
 from rest_framework.decorators import api_view
@@ -16,36 +16,42 @@ def home(request):
     return render(request, 'talk/index.html', tmpl_vars)
 
 
-@api_view(['GET', 'POST'])
-def post_collection(request):
-    if request.method == 'GET':
-        _posts = Post.objects.all()
-        serializer = PostSerializer(_posts, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        _data = {
-            'text': request.data.get('the_post'),
-            'author': request.user
-        }
-        serializer = PostSerializer(data=_data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serailizer.errors, status=status.HTTP_400_BAD_REQUEST)
+# ------- CLASS_BASED VIEWS ----------
 
 
-@api_view(['GET', 'DELETE'])
-def post_element(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return HttpResponse(status=404)
 
-    if request.method == 'GET':
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
 
-    elif request.method == 'DELETE':
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# ------- FUNCTIONAL VIEWS ----------
+
+
+# @api_view(['GET', 'POST'])
+# def post_collection(request):
+#     if request.method == 'GET':
+#         _posts = Post.objects.all()
+#         serializer = PostSerializer(_posts, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         _data = {
+#             'text': request.data.get('the_post'),
+#             'author': request.user
+#         }
+#         serializer = PostSerializer(data=_data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['GET', 'DELETE'])
+# def post_element(request, pk):
+#     post = get_object_or_404(Post, id=pk)
+#
+#     if request.method == 'GET':
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'DELETE':
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
