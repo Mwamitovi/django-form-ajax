370d5705f54566bc39dc68b6ebb35e84743be6ab
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 18:34:54 +0300

    Update urlpatterns, to fetch the class-based views, PostCollection() and PostMember()

--- part4/talk_project/talk/urls.py
+++ part4/talk_project/talk/urls.py
@@ -5,7 +5,10 @@ from talk import views
 
 urlpatterns = [
     url(r'^$', views.home),
-    # api
-    url(r'^api/v1/posts/$', views.post_collection),
-    url(r'^api/v1/posts/(?P<pk>[0-9]+)$', views.post_element),
+    # api-class_based-views
+    url(r'^api/v1/posts/$', views.PostCollection.as_view()),
+    url(r'^api/v1/posts/(?P<pk>[0-9]+)$', views.PostMember.as_view()),
+    # api-function-views
+    # url(r'^api/v1/posts/$', views.post_collection),
+    # url(r'^api/v1/posts/(?P<pk>[0-9]+)$', views.post_element),
 ]
