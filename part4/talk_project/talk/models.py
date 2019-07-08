7db02a33aa461be04a5b9a80ac2f69187fdfb1dc
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 17:07:35 +0300

    Create "Post" model

--- part4/talk_project/talk/models.py
+++ part4/talk_project/talk/models.py
@@ -1,3 +1,16 @@
+# talk/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    text = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.text + ' - ' + self.author.username
