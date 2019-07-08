5a71c4c2c9adb881c867f5003501eda499683d44
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 17:48:10 +0300

    Correct syntax error (serializer)

--- part3/talk_project/talk/views.py
+++ part3/talk_project/talk/views.py
@@ -32,7 +32,7 @@ def post_collection(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serailizer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'DELETE'])
