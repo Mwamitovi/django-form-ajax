dfd07670786a3ab7d72c7fd1239bee4041f38c8d
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 16:55:43 +0300

    Update the project urlpatterns

--- part4/talk_project/talk_project/urls.py
+++ part4/talk_project/talk_project/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login
+from talk_project import views
+
+admin.autodiscover()
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', login),
+    url(r'^logout/$', views.logout_page),
+    url(r'^', include('talk.urls')),
 ]
